53c15d26ec8bdaf7571cac4886e2233918b71184
Sesbian-Lex <cyrusjansacay13@gmail.com>
2026-03-01 14:22:15 +0800

    added laptops to portfolio

--- src/MovingSampleCard.jsx
+++ src/MovingSampleCard.jsx
@@ -1,12 +1,17 @@
 import './MovingSamples.css'
 import { useRef, useEffect } from 'react'
+import laptop from './assets/laptop.png'
 
 function MovingSampleCard({progress2, img, title, text, id}){
     const thisCard = useRef();
     
     return(
         <div className='moving-sample-card' id={id} ref={thisCard}>
-            <img src={img}/>
+
+            <img src={img} className='main-img'/>
+            <img src={laptop} className='disappearing-laptop'/>
+
+            
             <h2>
                 {title}
             </h2>
